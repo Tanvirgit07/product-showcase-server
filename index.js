14901ebcc5aeb26eb623ac8244f47922ac23fb9f
Tonmay1555 <ahmmedtonmay1555@gmail.com>
2024-08-18 14:54:07 +0600

    add item

--- index.js
+++ index.js
@@ -28,6 +28,8 @@ async function run() {
     const usersCollection = database.collection("users");
     const itemsCollection = database.collection("items");
 
+    
+
     app.post("/jwt", async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
@@ -151,8 +153,6 @@ async function run() {
     });
 
     
-
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
